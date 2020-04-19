60c7b749f58d83565e8fee9db7ad3b36aa193f49
mxmfshr <22074734+mxmfshr@users.noreply.github.com>
2020-04-19 20:35:44 +0300

    + reports

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,19 +1,18 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta
-
+from datetime import datetime, timedelta
 import os
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
+    'owner': 'mxmfshr',
     'depends_on_past': False,
-    'start_date': days_ago(2),
-    'email': ['airflow@example.com'],
+    'email': ['mxmfshr@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
+    'start_date': datetime(2018, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     # 'queue': 'bash_queue',
@@ -41,44 +40,27 @@ dag = DAG(
 )
 
 t1 = BashOperator(
-    task_id='print_date',
+    task_id='load_data',
     bash_command='papermill {{ airflow_home }}/scripts/get_data.ipynb - -p date {{ ds }} > /dev/null',
     dag=dag,
 )
 
 t2 = BashOperator(
-    task_id='sleep',
-    depends_on_past=False,
-    bash_command='sleep 5',
-    retries=3,
+    task_id='daily_report_notebook',
+    bash_command='papermill {{ airflow_home }}/scripts/eda.ipynb {{ airflow_home }}/reports/notebooks/report_{{ ds }}.ipynb -p data_path {{ airflow_home }}/data/output_{{ ds }}.csv',
     dag=dag,
 )
-
-templated_command = """
-{% for i in range(5) %}
-    echo "{{ ds }}"
-    echo "{{ macros.ds_add(ds, 7)}}"
-    echo "{{ params.my_param }}"
-{% endfor %}
-"""
 
 t3 = BashOperator(
-    task_id='templated',
-    depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
+    task_id='daily_report_html',
+    bash_command='jupyter nbconvert {{ airflow_home }}/reports/notebooks/report_{{ ds }}.ipynb --output {{ airflow_home }}/reports/html/report_{{ ds }} --to html --no-input',
     dag=dag,
 )
 
-dag.doc_md = __doc__
-
-t1.doc_md = """\
-#### Task Documentation
-You can document your task using the attributes `doc_md` (markdown),
-`doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-rendered in the UI's Task Instance Details page.
-![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-"""
-
-t1 >> [t2, t3]
+t4 = BashOperator(
+    task_id='daily_report_profiling',
+    bash_command='pandas_profiling {{ airflow_home }}/data/output_{{ ds }}.csv {{ airflow_home }}/reports/html/profiling_{{ ds }}.html',
+    dag=dag,
+)
+t1 >> t2 >> [t3, t4]
 
